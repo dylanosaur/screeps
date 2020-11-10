f9ae22119726eb904db7ed21984c6ab6db9f7ea3
Dylan Adams <dadams10642@outlook.com>
2020-11-10 00:32:19 -0600

    basic harvesters running for rcl 2

---
+++ role.extractor.js
@@ -0,0 +1,30 @@
+var roleHarvester = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        var rsi = parseInt(((creep.name.split('Worker')[1])/10)%2);
+        console.log(creep.name, rsi);
+	    if(creep.carry.energy < creep.carryCapacity) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[rsi]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[rsi], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+        else {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION ||
+                                structure.structureType == STRUCTURE_SPAWN ||
+                                structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                    }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+        }
+	}
+};
+
+module.exports = roleHarvester;
